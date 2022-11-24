469d961f3894d9b8b8d09824e233a3c221f387ab
AKI202202514 <AKI202202514@PC012422>
2022-11-24 11:51:18 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dice_face_5_problem
+{
+    public class Program
+    {
+
+        //Subroutine to output text
+         static void Output5()
+        {
+        Console.WriteLine("ooooooooooo");
+        Console.WriteLine("o         o");
+        Console.WriteLine("o  #   #  o");
+        Console.WriteLine("o    #    o");
+        Console.WriteLine("o  #   #  o");
+        Console.WriteLine("o         o");
+        Console.WriteLine("ooooooooooo");
+        }
+
+        // Main program
+        static void Main(string[]args)
+        {
+            Output5();
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
+
+
+    
+    
